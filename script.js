723aac6c253a718fcca630fa8624614df61517c1
Varinder Kaur <VK92@myscc.ca>
2022-09-22 18:36:04 -0400

    updated script.js

--- script.js
+++ script.js
@@ -2,19 +2,7 @@
 	WEB 303 Assignment 1 - jQuery
 	{Varinder Kaur}
 */
-/*$(document).ready(function()
-{
-$("input").change(function()
-{
- let salary= $("#yearly-salary").val();
- let percent= $("#percent").val();
 
-
- let result=((salary*percent)/100).toFixed(2);
- $("#amount").text("$" +result);
-
-});
-});*/
 
 $(document).ready(function()
 {
